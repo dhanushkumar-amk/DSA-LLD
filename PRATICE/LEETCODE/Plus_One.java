11ff6561f3987e3d99b9d22c066622eb1f43bd3c
dhanushkumar <dhanushkumaramk@gmail.com>
2026-01-01 21:21:30 +0530

    MODIFIED

---
+++ PRATICE/LEETCODE/Plus_One.java
@@ -0,0 +1,32 @@
+package PRATICE.LEETCODE;
+
+import java.util.Arrays;
+
+// problem name => leetcode  66
+// problem url => https://leetcode.com/problems/plus-one/description/?envType=daily-question&envId=2026-01-01
+
+
+public class Plus_One {
+    public static void main(String[] args) {
+        int[] digits = {9,5,9};
+        int[] ans = plusOne(digits);
+        System.out.println(Arrays.toString(ans));
+    }
+
+
+    public static int[] plusOne(int[] digits) {
+
+        for (int i = digits.length - 1; i >= 0; --i) {
+            if(digits[i] + 1 < 10){
+                digits[i]++;
+                return digits;
+            }
+            else{
+                digits[i] = 0;
+            }
+        }
+        int[] result =  new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
