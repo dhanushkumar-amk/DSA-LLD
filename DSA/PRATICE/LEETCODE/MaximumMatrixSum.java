cab5c365640988eede4af497b8c2bf8f836d65ca
dhanushkumar <dhanushkumaramk@gmail.com>
2026-01-05 19:09:35 +0530

    MODIFIED

---
+++ DSA/PRATICE/LEETCODE/MaximumMatrixSum.java
@@ -0,0 +1,18 @@
+package DSA.PRATICE.LEETCODE;
+
+public class MaximumMatrixSum {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3},
+                {-1,-2,-3},
+                {1,2,3}
+        };
+
+        long ans = maxMatrixSum(matrix);
+        System.out.println(ans);
+    }
+
+    public static long maxMatrixSum(int[][] matrix) {
+        return 1234l;
+    }
+}
---
+++ LLD/OOPS/Days.java
@@ -0,0 +1,7 @@
+package LLD.OOPS;
+
+ public enum Days {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+}
+
+
