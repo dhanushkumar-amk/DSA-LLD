cd879d41cc9dbd7407a878abf16ca917e3546399
dhanushkumar <dhanushkumaramk@gmail.com>
2026-01-20 20:46:09 +0530

    Learning  collection framework in java (priority queue, set and hashmap)

--- DSA/LEARN/COLLECTION_FRAMEWORK/QueueCollection.java
+++ DSA/LEARN/COLLECTION_FRAMEWORK/QueueCollection.java
@@ -47,5 +47,130 @@ public class QueueCollection {
         System.out.println(deque);
 
 
+        System.out.println("Priority Queue");
+        // priority queue
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.offer(1);
+        priorityQueue.offer(21);
+        priorityQueue.offer(3);
+        priorityQueue.offer(4);
+        priorityQueue.offer(0);
+
+
+        List<Integer> top2 = new LinkedList<>();
+        int index = 0;
+        while (!priorityQueue.isEmpty()){
+
+            if (index == 2)
+                break;
+            top2.add(priorityQueue.poll());
+            index++;
+        }
+
+        // default min heap
+        System.out.println(top2);
+        System.out.println(priorityQueue);
+
+
+        List<StudentMarks> studentMarks = new ArrayList<>();
+        // get the top3 student based on marks
+
+        studentMarks.add(new StudentMarks(20, 30));
+        studentMarks.add(new StudentMarks(70, 80));
+        studentMarks.add(new StudentMarks(50, 90));
+        studentMarks.add(new StudentMarks(80, 89));
+        studentMarks.add(new StudentMarks(74, 36));
+
+        PriorityQueue<StudentMarks> studentMarksPriorityQueue = new PriorityQueue<>(studentMarks);
+
+        List<StudentMarks> top3 = new LinkedList<>();
+        int indexRange = 0;
+        while (!studentMarksPriorityQueue.isEmpty()){
+
+            if (indexRange == 3)
+                break;
+            top3.add(studentMarksPriorityQueue.poll());
+            indexRange++;
+        }
+
+        System.out.println(top3);
+
+        // compartable class in an anamous function with lambda
+        PriorityQueue<Integer> priorityQueue2 = new PriorityQueue<>( (a, b) -> b - a);
+
+    }
+}
+
+class  StudentMarks implements Comparable<StudentMarks> {
+    private int maths;
+    private int science;
+
+    @Override
+    public String toString() {
+        return "StudentMarks{ \n " +
+                "maths=" + maths +
+                ", science=" + science +
+                '}' + "\n";
+    }
+
+    public int getMaths() {
+        return maths;
+    }
+
+    public void setMaths(int maths) {
+        this.maths = maths;
+    }
+
+    public int getScience() {
+        return science;
+    }
+
+    public void setScience(int science) {
+        this.science = science;
+    }
+
+    public StudentMarks(int maths, int science) {
+        this.maths = maths;
+        this.science = science;
+    }
+
+    @Override
+    public int compareTo(StudentMarks o) {
+        /*
+        * current object < other object
+        *   return negative number
+        *
+        * current object > other object
+        *   return positive number
+        *
+        * current object == other object
+        *   return 0;
+        * */
+
+        /*
+        if (this.maths < o.science)
+            return -1;
+
+        if (this.maths > o.maths)
+            return 1;
+
+        if (this.maths == o.maths)
+            return 0;
+       */
+
+//        this above three line can be return in one line
+        // for ascending order we use this.math - o.math
+        // for descending order we use o.math - this.math;
+
+        return o.maths - this.maths;
+    }
+
+}
+
+class MyCustomComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2 - o1; // descending order
     }
 }
