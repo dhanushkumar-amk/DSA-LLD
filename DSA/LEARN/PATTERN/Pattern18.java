87e81a42bb61d522ede1edd37e1a9f42d5a95dbe
dhanushkumar <dhanushkumaramk@gmail.com>
2026-01-24 04:50:02 +0530

    Learning Patterns from pattern  18  in DSA sheet

---
+++ DSA/LEARN/PATTERN/Pattern18.java
@@ -0,0 +1,17 @@
+package DSA.LEARN.PATTERN;
+
+public class Pattern18 {
+    public static void main(String[] args) {
+        pattern18(5);
+    }
+
+    private static void pattern18(int n) {
+
+        for (int i = 0; i < n; i++) {
+            for(char ch = (char) ('E' - i); ch <= 'E'; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+}
