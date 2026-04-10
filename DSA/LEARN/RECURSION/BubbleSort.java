2d02ade9e7dcf8468b217fade77d652a488de427
dhanushkumar <dhanushkumaramk@gmail.com>
2026-04-10 09:03:17 +0530

    recursion sorting

---
+++ DSA/LEARN/RECURSION/BubbleSort.java
@@ -0,0 +1,30 @@
+package DSA.LEARN.RECURSION;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,1};
+        System.out.println(" Before sorting = " + Arrays.toString(arr));
+
+        bubbleSort(arr, arr.length - 1, 0);
+        System.out.println("After sorting = " +Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] arr, int row, int col){
+        if (row == 0)
+            return;
+
+        if (col < row){
+            if (arr[col] > arr[col + 1]){
+                int temp = arr[col];
+                arr[col] = arr[col + 1];
+                arr[col + 1] = temp;
+            }
+
+            bubbleSort(arr, row , col + 1);
+        }else{
+            bubbleSort(arr, row - 1, 0);
+        }
+    }
+}
