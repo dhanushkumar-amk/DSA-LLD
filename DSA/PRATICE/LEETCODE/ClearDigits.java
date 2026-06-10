0b1a422f90549c7c61b5fa358ce42f7b579114bf
dhanushkumar <dhanushkumaramk@gmail.com>
2026-06-10 19:57:16 +0530

    modified

--- DSA/PRATICE/LEETCODE/ClearDigits.java
+++ DSA/PRATICE/LEETCODE/ClearDigits.java
@@ -1,5 +1,7 @@
 package DSA.PRATICE.LEETCODE;
 
+import java.util.Stack;
+
 public class ClearDigits {
     public static void main(String[] args) {
 
@@ -7,7 +9,20 @@ public class ClearDigits {
 
 
      public static String clearDigits(String s) {
+         Stack<Character> characterStack = new Stack<>();
 
+         for(char character : s.toCharArray()){
+             if (Character.isDigit(character))
+                 characterStack.pop();
+             else
+                 characterStack.push(character);
+         }
+
+         StringBuilder sb = new StringBuilder();
+         while (!characterStack.isEmpty()){
+             sb.append(characterStack.pop());
+         }
+         return sb.reverse().toString();
     }
 }
 
