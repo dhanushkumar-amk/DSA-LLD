bc99dd3790444fb9361a60b6bc8e74d3be3bf5f6
dhanushkumar <dhanushkumaramk@gmail.com>
2026-06-21 11:07:09 +0530

    neetcode 150

--- DSA/PRATICE/NEETCODE_150/BestTimeToBuyAndSellStock.java
+++ DSA/PRATICE/NEETCODE_150/BestTimeToBuyAndSellStock.java
@@ -1,4 +1,27 @@
 package DSA.PRATICE.NEETCODE_150;
 
 public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        int buyPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+
+            if (prices[i] < buyPrice) {
+                buyPrice = prices[i];
+            }
+
+            profit = Math.max(profit, prices[i] - buyPrice);
+        }
+
+        return profit;
+    }
 }
