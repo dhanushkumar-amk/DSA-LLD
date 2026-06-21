e4cfa3710adf449661296adccc4c49df780de21b
dhanushkumar <dhanushkumaramk@gmail.com>
2026-06-21 10:13:02 +0530

    neetcode 150

---
+++ DSA/PRATICE/NEETCODE_150/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,4 @@
+package DSA.PRATICE.NEETCODE_150;
+
+public class BestTimeToBuyAndSellStock {
+}
---
+++ DSA/PRATICE/NEETCODE_150/ContainerWithMostWater.java
@@ -0,0 +1,35 @@
+package DSA.PRATICE.NEETCODE_150;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+
+        int[] height = {1,8,6,2,5,4,8,3,7};
+
+        System.out.println(maxArea(height));
+    }
+
+    public static int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int maxArea = 0;
+
+        while (left < right) {
+
+            int width = right - left;
+            int length = Math.min(height[left], height[right]);
+            int area = length * width;
+
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
