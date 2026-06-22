a6b991d40e31185dca6213430016c24ee82761c9
dhanushkumar <dhanushkumaramk@gmail.com>
2026-06-22 20:19:16 +0530

    OOPs

---
+++ DSA/PRATICE/NEETCODE_150/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,11 @@
+package DSA.PRATICE.NEETCODE_150;
+
+public class LongestRepeatingCharacterReplacement {
+    public static void main(String[] args) {
+
+    }
+
+      public int characterReplacement(String s, int k) {
+
+    }
+}
