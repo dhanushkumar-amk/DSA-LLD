98c793626937576743d0c73245cd5bdbba845ba5
dhanushkumar <dhanushkumaramk@gmail.com>
2026-06-24 20:59:50 +0530

    OOPs

---
+++ LLD/OOPS/POLYMORPHISM/Shapes.java
@@ -0,0 +1,11 @@
+package LLD.OOPS.POLYMORPHISM;
+
+public class Shapes {
+    void area(){
+        System.out.println("i am in shapes");
+    }
+
+    void fun(){
+        System.out.println("fun");
+    }
+}
