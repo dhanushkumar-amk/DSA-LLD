c6db2aea2c8f9769c5b5b33223d72a3fd7334b09
dhanushkumar <dhanushkumaramk@gmail.com>
2026-07-01 21:25:14 +0530

    NeatCode 150

---
+++ DSA/PRATICE/NEETCODE_150/KokoEatingBananas.java
@@ -0,0 +1,40 @@
+package DSA.PRATICE.NEETCODE_150;
+
+import java.util.Arrays;
+
+public class KokoEatingBananas {
+    public static void main(String[] args) {
+
+    }
+
+    public static int minEatingSpeed(int[] arr, int hour) {
+         int low = 1;
+        int high = Arrays.stream(arr).max().getAsInt();
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            long totalHours = findTotalHoursWouldTakenByKoko(arr, mid);
+
+            if (totalHours <= hour) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private static long findTotalHoursWouldTakenByKoko(int[] arr, int mid) {
+
+        long totalHour = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            totalHour += (long) Math.ceil((double) arr[i] / mid);
+        }
+
+        return totalHour;
+    }
+}
