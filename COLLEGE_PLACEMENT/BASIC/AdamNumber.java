475de00cea617df585feff258bd597903351d1eb
dhanushkumar <dhanushkumaramk@gmail.com>
2026-07-29 20:00:09 +0530

    Modified

--- COLLEGE_PLACEMENT/BASIC/AdamNumber.java
+++ COLLEGE_PLACEMENT/BASIC/AdamNumber.java
@@ -17,26 +17,26 @@ public class AdamNumber {
     public static boolean checkAdamOrNot(int n) {
 //        example if n = 12
         // reverse orginal value 12 => 21
-        int reverseOfN = reverse(n);
+        int reverseOfN = reverseOfANumber(n);
 
         // square of the orginal value 12 * 12 => 144
-        int squareOfN = square(n);
+        int squareOfN = squareOfANumber(n);
         // Reverse the square of the orginal number 144 => 441
-        int reverseOfSquareOfN = reverse(squareOfN);
+        int reverseOfSquareOfN = reverseOfANumber(squareOfN);
 
         // Square the reversed number 21 * 21 => 441
-        int squareOfReverse = square(reverseOfN);
+        int squareOfReverse = squareOfANumber(reverseOfN);
 
         return reverseOfSquareOfN == squareOfReverse; // 441 == 441 true
     }
 
     //square function
-    public static int square(int n) {
+    public static int squareOfANumber(int n) {
         return n * n;
     }
 
     // revverse function
-    public static int reverse(int n) {
+    public static int reverseOfANumber(int n) {
         int sum = 0;
         while (n > 0) {
             int remainder = n % 10;
