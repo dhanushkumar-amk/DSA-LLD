d463b94507d0cc1c0176686d4cdb4192cd0ee2f7
dhanushkumar <dhanushkumaramk@gmail.com>
2026-08-09 20:31:54 +0530

    Modified

---
+++ DSA/LEETCODE/GREEDY/LongestPalindrome.java
@@ -0,0 +1,26 @@
+package DSA.LEETCODE.GREEDY;
+
+import java.util.HashMap;
+
+public class LongestPalindrome {
+
+     public int longestPalindrome(String s) {
+        int oddCount = 0;
+
+         HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            if (map.get(ch) % 2 == 1)
+                oddCount++;
+            else
+                oddCount--;
+        }
+
+        if (oddCount > 1)
+            return s.length() - oddCount + 1;
+
+        return s.length();
+    }
+
+}
