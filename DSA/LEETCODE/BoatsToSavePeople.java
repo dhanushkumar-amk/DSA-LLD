b17f95b839c8ea823eaeaf4178d4f18f071df86f
dhanushkumar <dhanushkumaramk@gmail.com>
2026-09-08 09:03:12 +0530

    Modified

---
+++ DSA/LEETCODE/BoatsToSavePeople.java
@@ -0,0 +1,24 @@
+package DSA.LEETCODE;
+
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+    public int numRescueBoats(int[] people, int limit) {
+        int count = 0;
+
+        Arrays.sort(people);
+
+        int left = 0;
+        int right = people.length - 1;
+
+        while (left <= right){
+            if (people[left] + people[right] <= limit)
+                left++;
+
+            right--;
+
+            count++;
+        }
+        return count;
+    }
+}
---
+++ DSA/LEETCODE/CountCommasInRange.java
@@ -0,0 +1,15 @@
+package DSA.LEETCODE;
+
+public class CountCommasInRange {
+    public int countCommas(int n) {
+        int count = 0;
+
+        if (n <= 999)
+            return 0;
+
+        for (int i = 1000; i <= n ; i++) {
+            count++;
+        }
+        return count;
+    }
+}
