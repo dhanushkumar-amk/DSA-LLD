066560e4e850dd5071424e0bcc37f7a34722b4e8
dhanushkumar <dhanushkumaramk@gmail.com>
2026-09-10 11:27:50 +0530

    Modified

---
+++ DSA/LEETCODE/IntersectionOfTwoArrays.java
@@ -0,0 +1,26 @@
+package DSA.LEETCODE;
+
+import java.util.HashSet;
+
+public class IntersectionOfTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for(int num : nums1)
+            set1.add(num);
+
+        for(int num : nums2){
+            if (set1.contains(num))
+                set2.add(num);
+        }
+
+        int[] ans = new int[set2.size()];
+
+        int i = 0;
+        for(int element : set2)
+            ans[i++] = element;
+
+        return ans;
+    }
+}
